2ab899361a121df73ec3e440f9bcfff13aa6df76
Perzi-MG <mgarciammej@gmail.com>
2025-02-28 19:21:27 -0600

    Added products

---
+++ src/products/products.service.ts
@@ -0,0 +1,74 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import {v4 as uuid} from 'uuid';
+import { CreateEmployeeDto } from 'src/employees/dto/create-employee.dto';
+
+@Injectable()
+export class ProductsService {
+  private products: CreateProductDto[] = [
+    {
+      productId: uuid(),
+      productName: 'Sabritas normal',
+      price: 29,
+      countSeal: 3,
+      provider: uuid()
+    },
+    {
+      productId: uuid(),
+      productName: 'Coca cola 600ml',
+      price: 40,
+      countSeal: 2,
+      provider: uuid()
+    },
+    {
+      productId: uuid(),
+      productName: 'Agua Ciel',
+      price: 15,
+      countSeal: 2,
+      provider: uuid()
+    }
+  ];
+  create(createProductDto: CreateProductDto) {
+    createProductDto.productId = uuid();
+    this.products.push(createProductDto);
+    return createProductDto;
+  }
+
+  findAll() {
+    return this.products;
+  }
+
+  findOne(id: string) {
+    const productFound = this.products.filter((product) => product.productId === id)[0];
+    if(!productFound) throw new NotFoundException();
+    return productFound;
+  }
+
+  findByProvider(id: string) {
+    const productFound = this.products.filter((product) => product.provider === id);
+    if(!productFound) throw new NotFoundException();
+    return productFound;
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    let productToUpdate = this.findOne(id);
+    productToUpdate = {
+      ...productToUpdate,
+      ...updateProductDto
+    };
+    this.products = this.products.map((product) => {
+      if(product.productId === id){
+        product = productToUpdate;
+      }
+      return productToUpdate;
+    });
+    return productToUpdate;
+  }
+
+  remove(id: string) {
+    this.findOne(id);
+    this.products = this.products.filter((product) => product.productId !== id);
+    return this.products;
+  }
+}
