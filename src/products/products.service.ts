7fc209e008e818678bef3a7c869ec5bfa982c1ce
Perzi-MG <mgarciammej@gmail.com>
2025-03-01 13:33:51 -0600

    Created provider resource

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,6 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import {v4 as uuid} from 'uuid';
-import { CreateEmployeeDto } from 'src/employees/dto/create-employee.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
@@ -13,29 +11,6 @@ export class ProductsService {
     @InjectRepository(Product)
     private productRepository: Repository<Product>
   ){}
-  private products: CreateProductDto[] = [
-    {
-      productId: uuid(),
-      productName: 'Sabritas normal',
-      price: 29,
-      countSeal: 3,
-      provider: uuid()
-    },
-    {
-      productId: uuid(),
-      productName: 'Coca cola 600ml',
-      price: 40,
-      countSeal: 2,
-      provider: uuid()
-    },
-    {
-      productId: uuid(),
-      productName: 'Agua Ciel',
-      price: 15,
-      countSeal: 2,
-      provider: uuid()
-    }
-  ];
   create(createProductDto: CreateProductDto) {
     const product = this.productRepository.save(createProductDto)
     return product;
@@ -53,9 +28,7 @@ export class ProductsService {
   
 
   findByProvider(id: string) {
-    const productFound = this.products.filter((product) => product.provider === id);
-    if(!productFound) throw new NotFoundException();
-    return productFound;
+    return "Ok"
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
