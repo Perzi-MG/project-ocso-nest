5f079c9e1e77231b4022c179e1a303c51a70fa4d
Perzi-MG <mgarciammej@gmail.com>
2025-04-01 15:32:46 -0600

    feat: Cookies in back

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { TOKEN_NAME } from './constants/jwt.constants';
+import { Cookies } from './Decorators/cookies.decorator';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -16,8 +19,18 @@ export class AuthController {
   }
 
   @Post("login")
-  login(@Body() loginUserDto: LoginUserDto){
-    return this.authService.loginUser(loginUserDto)
+  async login(@Body() loginUserDto: LoginUserDto, @Res({passthrough: true}) response: Response, @Cookies() cookies: any){
+    const token =  await this.authService.loginUser(loginUserDto)
+    let expireDate = new Date()
+    expireDate.setDate(expireDate.getDay() + 7)
+    response.cookie(TOKEN_NAME, token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      expires: expireDate,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    })
+    return;
   }
   @Patch("/:email")
   updateUser(@Param('email') userEmail: string ,@Body() updateUserDto: UpdateUserDto){
