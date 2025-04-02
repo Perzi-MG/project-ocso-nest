a9e549f3c503c94ed8b5846361dd94bcca42a053
Perzi-MG <mgarciammej@gmail.com>
2025-04-01 19:18:30 -0600

    fix: return location on create

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -23,6 +23,8 @@ export class LocationsService {
     const location = this.locationRepository.findOneBy({
       locationId: id,
     })
+    if(!location) throw new NotFoundException("Location not found");
+    return location;
   }
 
   async update(id: number, updateLocationDto: UpdateLocationDto) {
