947c9f1a195afdfb59f1e75288e6bcb4f3f473a7
Perzi-MG <mgarciammej@gmail.com>
2025-04-07 21:48:39 -0600

    Correccion Return findOne Manager

--- src/managers/managers.service.ts
+++ src/managers/managers.service.ts
@@ -21,10 +21,14 @@ export class ManagersService {
   }
 
   findOne(id: string) {
-    const manager = this.managerRepository.findOneBy({
-      managerId: id
+    const manager = this.managerRepository.findOne({
+      where: {managerId: id},
+      relations: {
+        location: true,
+      }
     })
     if(!manager) throw new NotFoundException('No manager found');
+    return manager;
 
   }
 
